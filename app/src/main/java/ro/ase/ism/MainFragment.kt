ee377e040d5d22822b705bc9ebe93c752c0bf052
Mihai Pavel <pavelmihailvaleriu@gmail.com>
2024-05-30 12:34:40 +0300

    made ui

---
+++ app/src/main/java/ro/ase/ism/MainFragment.kt
@@ -0,0 +1,30 @@
+package ro.ase.ism
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+    }
+}
---
+++ app/src/main/java/ro/ase/ism/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ro.ase.ism
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
